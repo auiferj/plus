779957cd3c9a49752627fe8d98637451da241f9d
zhangwei <wmbackj@163.com>
2020-10-25 17:53:51 +0800

    lambda表达式创建集合

--- src/main/java/com/wkcto/plus/mapper/UserMapper.java
+++ src/main/java/com/wkcto/plus/mapper/UserMapper.java
@@ -5,6 +5,8 @@ import com.wkcto.plus.entity.User;
 
 /**
  * BaseMapper中提供了17个方法
+ * BaseMapper是UserMapper的父接口
+ * 可以在UserMapper中直接调用BaseMapper中的方法：this.BaseMapper.方法
  */
 public interface UserMapper extends BaseMapper<User> {
 }
--- src/test/java/com/wkcto/plus/PlusApplicationTests.java
+++ src/test/java/com/wkcto/plus/PlusApplicationTests.java
@@ -6,6 +6,11 @@ import com.wkcto.plus.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SuppressWarnings("all")
 @SpringBootTest
 class PlusApplicationTests {
@@ -21,6 +26,15 @@ class PlusApplicationTests {
         user.setEmail("zhangsan@sina.com");
         int insert = userDao.insert(user);
         System.out.println(insert);
+    }
+
+    @Test
+    void deleteByBatchId() {
+        //使用lambda表达式创建集合
+        List<Integer> ids = Stream.of(1, 2, 3, 4, 5).collect(Collectors.toList());
+        int i = userDao.deleteBatchIds(ids);
+        System.out.println(i);
+
     }
 
 }
